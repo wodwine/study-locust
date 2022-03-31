e885f2a32c36ea8ca2eb7048f4847e4108080079
APPMAN M-094 <admin@APPMAN-M-094.local>
2022-03-31 09:58:18 +0700

    User, HttpUser, TaskSet

--- 01_introduction/TaskSet/httpcat.py
+++ 01_introduction/TaskSet/httpcat.py
@@ -1,5 +1,5 @@
 from locust import TaskSet, constant, task, HttpUser
-
+from random import randint
 
 class MyHTTPCat(TaskSet):
 
@@ -8,7 +8,18 @@ class MyHTTPCat(TaskSet):
         self.client.get("/200")
         print("Get status of 200")
 
+    @task
+    def get_random_status(self):
+        status_codes = [100, 101, 102, 200, 201, 202, 203, 204, 205, 206, 207, 208, 226, 300, 301, 302, 303, 304, 305,
+                        307, 308, 400, 401, 402, 403, 404, 405, 406, 407, 408, 409, 410, 411, 412, 413, 414, 415, 416,
+                        417, 418, 421, 422, 423, 424, 426, 428, 429, 431, 444, 451, 499, 500, 501, 502, 503, 504, 505,
+                        506, 507, 508, 510, 511, 599]
+        status = status_codes[randint(0,len(status_codes))]
+        self.client.get(f"/{status}")
+        print(f"Get status of {status}")
+
 
 class MyLoadTest(HttpUser):
     host = "https://http.cat"
+    tasks = [MyHTTPCat]
     wait_time = constant(1)
